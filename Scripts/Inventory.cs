3c579c212e568492fccb7a6529d8b66f61079f67
kjtkanaly <kjtkanaly@gmail.com>
2023-10-14 22:55:08 -0400

    Starting to work on inventory system.

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Inventory : Node3D
+{
+    //-------------------------------------------------------------------------
+    // Game Componenets
+
+    // Godot Types
+	List<Node3D> inventory = new List<Node3D>();
+
+    // Basic Types
+
+    //-------------------------------------------------------------------------
+    // Game Events
+
+    //-------------------------------------------------------------------------
+    // <Inventory> Methods
+
+    //-------------------------------------------------------------------------
+    // Demo Methods
+}
