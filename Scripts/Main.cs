da3c38452f976daf5c3881489f170ca27ac725b9
kjtkanaly <kjtkanaly@gmail.com>
2023-10-14 22:04:58 -0400

    Added basic game pausing feature.

---
+++ Scripts/Main.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Main : Node3D
+{
+	//-------------------------------------------------------------------------
+	// Game Componenets
+
+	// Godot Types
+
+	// Basic Types
+
+	//-------------------------------------------------------------------------
+	// Game Events
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
+	public override void _Process(double delta)
+	{
+		TogglePause();
+	}
+
+	//-------------------------------------------------------------------------
+	// Main Methods
+	public void TogglePause() {
+		if (Input.IsActionJustPressed("Pause Game")) {
+			if (!GetTree().Paused) {
+				Input.MouseMode = Input.MouseModeEnum.Visible;
+				GetTree().Paused = true;
+			} else {
+				Input.MouseMode = Input.MouseModeEnum.Captured;
+				GetTree().Paused = false;
+			}
+		}
+	}
+
+	//-------------------------------------------------------------------------
+	// Demo Methods
+}
