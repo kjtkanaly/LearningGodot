5e4545b9eda2da2b1189a629bdbe6f6b99de6823
kjtkanaly <kjtkanaly@gmail.com>
2023-10-16 15:44:27 -0400

    Working on inventory stuff

--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -1,27 +1,20 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
-public partial class Inventory : Node3D
+public partial class Item : Node3D
 {
     //-------------------------------------------------------------------------
     // Game Componenets
 
     // Godot Types
-	List<List<Item>> inventory = new List<List<Item>>();
 
     // Basic Types
-	const int inventorySlots = 30;
 
     //-------------------------------------------------------------------------
     // Game Events
-	public override void _Ready()
-	{
-		inventory = new List<List<Item>>(inventorySlots);
-	}
 
     //-------------------------------------------------------------------------
-    // <Inventory> Methods
+    // Item Methods
 
     //-------------------------------------------------------------------------
     // Demo Methods
